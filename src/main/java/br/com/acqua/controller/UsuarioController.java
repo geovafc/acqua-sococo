2c5a3682bd17e91f3c7c8d519034bc2656874aaf
Rayan Teixeira <rayanteixeira91@gmail.com>
2017-07-18 22:15:33 -0300

    Alterações de usuário

--- src/main/java/br/com/acqua/controller/UsuarioController.java
+++ src/main/java/br/com/acqua/controller/UsuarioController.java
@@ -1,15 +1,26 @@
 package br.com.acqua.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.acqua.entity.User;
+import br.com.acqua.entity.UserRole;
 import br.com.acqua.service.UsuarioService;
+
 
 @Controller
 @RequestMapping("/usuarios")
@@ -22,10 +33,39 @@ public class UsuarioController {
 	
 	@GetMapping("/novo")
 	public ModelAndView novo(){
+		User usuario = new User();
+		/*usuario.setCodigo("001");
+		
+		try {
+			String date_s = "2011-01-18"; 
+			SimpleDateFormat dt = new SimpleDateFormat("yyyyy-mm-dd"); 
+			Date date = dt.parse(date_s);
+			usuario.setDataCadastro(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+			
+		usuario.setEnabled(true);
+		usuario.setNome("teste");
+		usuario.setPassword("234");
+		usuario.setSobrenome("teste");
+		usuario.setUsername("rraa");
+		
+		usuarioService.salvar(usuario);*/
+		
+		
 		
 		ModelAndView view = new ModelAndView(CADASTRO_VIEW);
-		view.addObject(new User());
+		view.addObject("usuario", usuario);
+		
+		
+		
 		return view;
+		
+	
+		
+		
 	}
 	
 	@GetMapping
@@ -36,5 +76,31 @@ public class UsuarioController {
 		return view;
 	}
 	
+	
+	@ModelAttribute("todosPerfis")
+	public List<UserRole> todosStatusTitulo() {
+		return Arrays.asList(UserRole.values());
+	}
+	
+	@RequestMapping(method = RequestMethod.POST )
+	public String salvar(@Validated User usuario, Errors errors, RedirectAttributes attributes  ){
+		
+		if(errors.hasErrors()){
+			return CADASTRO_VIEW;
+		}
+		
+		try{
+			
+			Date hoje = new Date();
+			usuario.setDataCadastro(hoje);
+				
+			usuarioService.salvar(usuario);
+			attributes.addFlashAttribute("mensagem", "Operador cadastrado com sucesso!");
+			return "redirect:/usuarios";
+		}catch (IllegalArgumentException e) {
+			attributes.addFlashAttribute("mensagem", "Desculpe, mas algo deu errado.");
+			return CADASTRO_VIEW;
+		}
+	}
 	
 }
--- src/main/java/br/com/acqua/service/UsuarioService.java
+++ src/main/java/br/com/acqua/service/UsuarioService.java
@@ -3,6 +3,7 @@ package br.com.acqua.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.acqua.entity.User;
@@ -16,5 +17,14 @@ public class UsuarioService {
 	
 	public List<User> listar() {
 		return usuarioRepository.findAll();
+	}
+	
+	
+	public void salvar(User usuario){
+		try{			
+			usuarioRepository.save(usuario);			
+		}catch (DataIntegrityViolationException e) {
+			throw new IllegalArgumentException ("Algo deu errado ao salvar este operador");
+		}
 	}
 }
