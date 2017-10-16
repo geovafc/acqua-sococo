62880a63cd3070a4a83b32857e85046f47879911
Geovane Freitas <geovane.freitasc@gmail.com>
2017-10-15 21:05:53 -0300

    Update MovimentacaoService.java
    Correcao do problema de remover os outros perfis do usuario.

--- src/main/java/br/com/acqua/service/MovimentacaoService.java
+++ src/main/java/br/com/acqua/service/MovimentacaoService.java
@@ -1,9 +1,8 @@
 package br.com.acqua.service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,125 +10,70 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import br.com.acqua.dto.MovimentacaoMesAnoDTO;
-import br.com.acqua.entity.Movimentacao;
-import br.com.acqua.entity.Produto;
+import br.com.acqua.entity.Permissao;
 import br.com.acqua.entity.Usuario;
-import br.com.acqua.repository.MovimentacaoRepository;
-import br.com.acqua.repository.ProdutoRepository;
+import br.com.acqua.repository.PermissaoRepository;
 import br.com.acqua.repository.UserRepository;
-import br.com.acqua.repository.filter.MovimentacaoFilter;
 
 @Service
-public class MovimentacaoService {
+public class UsuarioService {
 
 	@Autowired
-	private MovimentacaoRepository movimentacaoRepository;
+	private UserRepository usuarioRepository;
+
 	@Autowired
-	private ProdutoRepository produtoRepository;
-	@Autowired
-	private UserRepository userRepository;
+	private PermissaoRepository permissaoRepository;
 
-	public void salvar(Movimentacao movimentacao, String userName) {
+	public Page<Usuario> findByPagination(int page, int size) {
+		Pageable pageable = new PageRequest(page, size);
+		return usuarioRepository.findAllByOrderByIdAsc(pageable);
+	}
 
-		Usuario user = userRepository.findByUsername(userName);
-
+	public void salvar(Usuario usuario) {
 		try {
-			movimentacao.setUsuario(user);
-			if (movimentacao.getDataHora() == null) {
-				movimentacao.setDataHora(new Date(System.currentTimeMillis()));
+			
+			List<Permissao> permissaos = new ArrayList<>();
+
+
+			if (usuario.getId() == null) {
+
+				permissaos.add(permissaoRepository.findByNome("ROLE_USER"));
+
+				if (usuario.getDataCadastro() == null) {
+					usuario.setDataCadastro(Date.valueOf(LocalDate.now()));
+					usuario.setEnabled(Boolean.TRUE);
+				}
+
+			} else {
+				
+				System.out.println(usuarioRepository.findByUsername(usuario.getUsername()).getPermissoes().size());
+				permissaos.addAll(usuarioRepository.findByUsername(usuario.getUsername()).getPermissoes());
+
 			}
-			movimentacaoRepository.save(movimentacao);
+			
+			usuario.setEnabled(Boolean.TRUE);
+
+			usuario.setPermissaos(permissaos);
+			
+			String hash = new BCryptPasswordEncoder().encode(usuario.getPassword());
+
+			usuario.setPassword(hash);
+
+			usuarioRepository.save(usuario);
 
 		} catch (DataIntegrityViolationException e) {
-			throw new IllegalArgumentException("Formato de data inválido");
+			throw new IllegalArgumentException("Algo deu errado ao salvar este operador");
 		}
 	}
 
-	// Ta pegando so pelo ano de 2017
-	public List<MovimentacaoMesAnoDTO> getCountMovimentacoesByMesAno() {
-		MovimentacaoMesAnoDTO objeto;
-		List<MovimentacaoMesAnoDTO> projetosAtivos = new ArrayList<>();
-		Integer ano = 2017;
-		HashMap<Integer, String> mesPorNumero = new HashMap<>();
-		mesPorNumero.put(1, "01");
-		mesPorNumero.put(2, "02");
-		mesPorNumero.put(3, "03");
-		mesPorNumero.put(4, "04");
-		mesPorNumero.put(5, "05");
-		mesPorNumero.put(6, "06");
-		mesPorNumero.put(7, "07");
-		mesPorNumero.put(8, "08");
-		mesPorNumero.put(9, "09");
-		mesPorNumero.put(10, "10");
-		mesPorNumero.put(11, "11");
-		mesPorNumero.put(12, "12");
-
-		for (int i = 1; i < 13; i++) {
-
-			objeto = new MovimentacaoMesAnoDTO();
-			Long quantidadeMovimentacoes = movimentacaoRepository.countByMovimentacoesFromMesAno(i, ano);
-
-			objeto.setMes(ano.toString() + "-" + mesPorNumero.get(i));
-			objeto.setQuantidadeMovimentacoes(quantidadeMovimentacoes);
-			objeto.setAno(Calendar.YEAR + "");
-			projetosAtivos.add(objeto);
-
-		}
-
-		return projetosAtivos;
-
+	public void delete(Long id) {
+		usuarioRepository.delete(id);
 	}
 
-	public Page<Movimentacao> findByPagination(int page, int size) {
-		Pageable pageable = new PageRequest(page, size);
-		return movimentacaoRepository.findAllByOrderByIdAsc(pageable);
+	public Usuario findById(Long id) {
+		return usuarioRepository.findOne(id);
 	}
-
-	public static Date converterDateFim(Date date) {
-		Calendar c = Calendar.getInstance();
-		c.setTime(date);
-		c.add(Calendar.HOUR, 23);
-		c.add(Calendar.MINUTE, 59);
-		return c.getTime();
-	}
-
-	public List<Movimentacao> listar() {
-		return movimentacaoRepository.findAll();
-	}
-
-	public Page<Movimentacao> pesquisar(MovimentacaoFilter filter, int page, int size) throws Throwable {
-
-		filter = atualizarFilter(filter);
-
-		Pageable pageable = new PageRequest(page, size);
-
-		return movimentacaoRepository.filtrar(filter, pageable);
-	}
-
-	private MovimentacaoFilter atualizarFilter(MovimentacaoFilter filter) {
-		
-		Produto produto = produtoRepository.findByCodigoDeBarras(filter.getCodigo());
-
-		filter.setProduto(produto);
-		
-		filter.setFim(filter.getFim() == null ? null : converterDateFim(filter.getFim()));
-	
-		Usuario usuario = userRepository.findByNome(filter.getNomeUsuario().toLowerCase());
-		
-		filter.setUsuario(usuario);
-		
-		return filter;
-	}
-
-	public void excluir(Long id) {
-		movimentacaoRepository.delete(id);
-	}
-
-	public Movimentacao buscar(Long id) {
-		return movimentacaoRepository.findOne(id);
-	}
-
 }
